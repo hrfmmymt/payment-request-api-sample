1c20076eba897bd659460ddb623e2cd741e44274
hrfmmymt <hrfmmymt@gmail.com>
2017-10-20 00:08:32 +0900

    modified.

--- app.js
+++ app.js
@@ -1,88 +1,69 @@
-function initPaymentRequest() {
-  let networks = [
-    'amex',
-    'diners',
-    'jcb',
-    'mastercard',
-    'visa'
-  ];
-
-  let types = [
-    'debit',
-    'credit',
-    'prepaid'
-  ];
-
-  let supportedInstruments = [{
-    supportedMethods: networks,
-  }, {
+function pay(details) {
+  const methodData = [{
     supportedMethods: ['basic-card'],
     data: {
-      supportedNetworks: networks,
-      supportedTypes: types
-    },
+      supportedNetworks: [
+        'visa',
+        'mastercard',
+        'amex',
+        'diners',
+        'jcb'
+      ]
+    }
   }];
 
-  let details = {
+  const options = {};
+
+  const request = new PaymentRequest(methodData, details, options);
+
+  request.show().then(result => {
+    return Promise.race([
+      fetch('/pay', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(result.toJSON())
+      })
+    ])
+    .then(response => {
+      if (response.status === 200) {
+        return result.complete('success');
+      } else {
+        return result.complete('fail');
+      }
+    })
+    .catch(() => result.complete('fail'));
+  });
+}
+
+function onBuyClicked(event) {
+  pay({
     displayItems: [{
-      label: 'Price',
+      label: event.target.parentNode.dataset.label,
       amount: {
         currency: 'JPY',
-        value: '100'
-      },
+        value: event.target.parentNode.dataset.value
+      }
     }, {
-      label: 'Discount',
+      label: 'Friends and family discount',
       amount: {
         currency: 'JPY',
-        value: '-10'
+        value : '-10'
       },
+      pending: true
     }],
     total: {
       label: 'Total',
       amount: {
         currency: 'JPY',
-        value: '90'
+        value: Number(event.target.parentNode.dataset.value) - 10
       }
-    },
-  };
-  return new PaymentRequest(supportedInstruments, details);
-}
-
-function onBuyClicked(request) {
-  request.show().then(instrumentResponse => {
-    sendPaymentToServer(instrumentResponse);
-  })
-  .catch(function(err) {
-    console.log(err);
+    }
   });
 }
 
-function sendPaymentToServer(instrumentResponse) {
-  window.setTimeout(() => {
-    instrumentResponse.complete('success')
-    .then(() => {
-      document.getElementById('result').innerHTML = instrumentToJsonString(instrumentResponse);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  }, 2000);
-}
-
-function instrumentToJsonString(instrument) {
-  let details = instrument.details;
-  details.cardNumber = 'XXXX-XXXX-XXXX-' + details.cardNumber.substr(12);
-  details.cardSecurityCode = '***';
-
-  return JSON.stringify({
-    methodName: instrument.methodName,
-    details: details,
-  }, undefined, 2);
-}
-
-const payButton = document.getElementById('buyButton');
-let request = initPaymentRequest();
-payButton.addEventListener('click', () => {
-  onBuyClicked(request);
-  request = initPaymentRequest();
+document.querySelectorAll('button[type="button"]').forEach(button => {
+  button.addEventListener('click', onBuyClicked);
 });
